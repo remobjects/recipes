44bebf3c7570d7fd5d4f3d11cac2cb0aa0548476
Carlo Kok <ck@remobjects.com>
2019-12-06 10:18:02 +0100

    0: Ultralight import definition

---
+++ ultralight/README.txt
@@ -0,0 +1,3 @@
+https://ultralig.ht/ is a lightweight and fast option for html based uis. See https://github.com/ultralight-ux/ultralight#ultralight for the readme/licensing info and binaries.
+
+to import, copy the include/ dir from the distribution to include/.
---
+++ ultralight/limits.h
@@ -0,0 +1,14 @@
+
+#define CHAR_BIT 8
+#define SHRT_MIN    (-32768)      
+#define SHRT_MAX      32767         
+#define USHRT_MAX     0xffff        
+#define INT_MIN     (-2147483647-1) 
+#define INT_MAX       2147483647 
+#define UINT_MAX      0xffffffff 
+#define LONG_MIN    (-2147483647L - 1) 
+#define LONG_MAX      2147483647L  
+#define ULONG_MAX     0xffffffffUL 
+#define LLONG_MAX     9223372036854775807i64   
+#define LLONG_MIN   (-9223372036854775807i64-1)
+#define ULLONG_MAX    0xffffffffffffffffui64   
