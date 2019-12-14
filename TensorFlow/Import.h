dce07da26a5d824a6f69a445fc9848e74d21fcaf
marc hoffman <mh@remobjects.com>
2019-12-14 15:46:40 -0400

    TensorFlow

---
+++ TensorFlow/Import.h
@@ -0,0 +1,14 @@
+﻿#import "include/tensorflow/c/c_api.h"
+
+//
+// Tasks
+//
+// in Settings, pick the appropriate Mode (Toffee or Island), SubMode and SDK for the
+// import. Also set the ImportSearchPath to contain the folder or folders that congain
+// your header files
+//
+// In this file, add #import clauses for each header you want to import
+//
+// Finally, drag any binaries that need to be linked to use the imported library into
+// the project and make sure their type is set to "ImportLinkLibrary".
+//
